241b7265736d6da6fd39fedfadd83e8fac8bae75
naveensachin <31157897+naveensachin@users.noreply.github.com>
2017-08-22 15:57:27 +0530

    Create POSSITIVE.C

---
+++ POSSITIVE.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    
+
+    if(num < 0)
+    {
+        printf("Number is NEGATIVE");
+    }
+    else if(num > 0)
+    {
+        printf("Number is POSSITIVE");
+    }
+
+    return 0;
+}
